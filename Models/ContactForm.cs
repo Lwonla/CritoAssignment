d3a052e07f42584a84261a68dc5e5d3d9e56376a
Leon Lagergren <leon.lagergren@utb.ecutbildning.se>
2023-10-06 12:40:45 +0200

    Project partial added

--- Models/ContactForm.cs
+++ Models/ContactForm.cs
@@ -4,14 +4,14 @@ namespace Crito.Models;
 
 public class ContactForm
 {
-    [Required]
+    [Required(ErrorMessage = "You must enter a name")]
     public string Name { get; set; } = null!;
     
-    [Required]
+    [Required(ErrorMessage = "You must enter an email address")]
     [EmailAddress]
     public string Email { get; set; } = null!;
 
-    [Required]
+    [Required(ErrorMessage = "You must enter a message")]
     public string Message { get; set; } = null!;
     public string? RedirectUrl { get; set; } = "/";
 }
--- wwwroot/scripts/script.js
+++ wwwroot/scripts/script.js
@@ -1,2 +1,21 @@
+function validateText(event) {
+    const nameInput = event.target;
+    const nameError = document.getElementById("nameError");
+    const nameRegex = /^[A-Za-z\s]+$/;
 
-                        
+    if (!nameInput.value.match(nameRegex)) {
+        nameError.textContent = "Please enter a valid name (letters and spaces only).";
+    } else {
+        nameError.textContent = "";
+    }
+}
+
+
+/* const validateText = (event) => {
+    if (event.target.value.length > 2) {
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = ""
+    }
+    else {
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Invalid length"
+    }
+} */
